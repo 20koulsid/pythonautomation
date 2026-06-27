cf57cbb35a6997ced93a58201d001446919c5a82
sidharth koul <167965443+20koulsid@users.noreply.github.com>
2026-06-27 23:32:42 +0530

    added module

---
+++ LearningPython/modules/Larger_num.py
@@ -0,0 +1,6 @@
+def larger_num(num1, num2):
+    if num1 > num2:
+        print(f"{num1} is larger than {num2}")
+    else:
+        print(f"{num2} is larger than {num1}")
+
---
+++ LearningPython/modules/Odd_Even.py
@@ -0,0 +1,5 @@
+def check_num(num):
+    if num % 2 == 0:
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
